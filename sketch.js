da1380d258950b47114153c2f016f471898f4c19
SamuelDRichards <Sam@samrichardsstudio.com>
2023-09-25 10:09:51 +1300

    fish
    did some work on the fish

--- sketch.js
+++ sketch.js
@@ -1,11 +1,11 @@
-let x = -100; //adjust the postion of the fish on the x - axis
-let y = 100; //adjust the postion of the fish on the y - axis
+let x = 150; //adjust the postion of the fish on the x - axis
+let y = 150; //adjust the postion of the fish on the y - axis
 let face_be_happy = 1; //if you make the number 1 or larger it makes the fish happy, anything less than 1 makes the fish sad, ONLY WORKS ON NOT EVIL FISH
 let fin = 1; //this turns the main body fin on or off, 1 or anything more than 1 is on, anything less is off
 
 
 function setup() {
-  createCanvas(200, 200);
+  createCanvas(300, 300);
    
   angleMode(DEGREES)
 }
@@ -18,14 +18,16 @@ function setup() {
 
 function draw() {
 
-  background(226, 208, 239) 
+  background(0) 
+
+  // rotate(290)
 
   fishnotevil(x, y, face_be_happy, fin)
 
-  //x++
-   if(x > 300){
-    x = -100
-   }
+  // x++
+  //  if(x > 300){
+  //   x = -100
+  //  }
   
 }
 
@@ -61,7 +63,7 @@ function fishnotevil(x, y, face_be_happy, fin) { //the totally definitely not ev
   triangle(x - 50, y + .5, x - 44, y - 6, x - 42, y + .5); //read top body fin
 
   fill(0); //black fill for the eye
-  circle(x + 60, y + 5, 5); //eye
+  circle(x + 55, y + 5, 5); //eye
 
   fill(54, 79, 110); //reseting the fill to the blue
 
@@ -71,19 +73,19 @@ function fishnotevil(x, y, face_be_happy, fin) { //the totally definitely not ev
 
   arc(x, y + 13, 98, 10, 0, 180); //body line
 
-  if (face_be_happy >= 1) {
-    arc(x + 60, y + 13, 10, 10, 100, 170); //happy mouth
-  } else {
-    arc(x + 63, y + 13, 10, 5, 180, 350); //sad mouth
-  }
+  arc(x + 73, y + 5, 15, 5, 100, 170); //happy mouth
+
+  push()
+
+  fill(229, 216, 189)
+
+  arc(x + 73, y + 5, 15, 15, 100, 170);
+
+  pop()
 
 
-
-  if (fin >= 1) { //if statment to make the fin show up or not 
-    strokeWeight(1);
-
-    triangle(x + 50, y + 10, x + 48, y + 15, x + 30, y + 18); //body fin
-  }
+  strokeWeight(1);
+  triangle(x + 50, y + 10, x + 48, y + 15, x + 30, y + 18); //body fin
 
 
 
@@ -94,10 +96,10 @@ function fishnotevil(x, y, face_be_happy, fin) { //the totally definitely not ev
   strokeWeight(2); //setting the stroke weight so the brim as some weight to it
   stroke(255, 72, 165); //pink color for the hat 
 
-  arc(x + 57, y - 7, 15, 15, 35, 140); //brim of the hat
+  arc(x + 52, y - 7, 15, 15, 35, 140); //brim of the hat
 
   fill(255, 72, 165); //pink color for the hat 
 
-  triangle(x + 55, y - .5, x + 57, y - 8, x + 59, y - .5); //tringle of the hat
+  triangle(x + 50, y - .5, x + 52, y - 8, x + 54, y - .5); //tringle of the hat
 
 }
